f991d92494769a40e23d817276727fca1538c54d
Gael Ramos <esgaelramos@gmail.com>
2023-12-31 22:58:15 -0600

    feat: create loader config for project, reading any enviorment, and the yaml with his relationship

---
+++ src/core/config.py
@@ -0,0 +1,44 @@
+"""Module `config` for load the application configuration."""
+
+from typing import Dict
+
+import yaml
+from decouple import config
+
+
+class AppConfig:
+    """Class `AppConfig` for load the application configuration."""
+
+    _instance = None
+
+    def __new__(cls):
+        """Create a new instance of AppConfig.
+
+        Verify if the instance is already created, if not, create a new one.
+        If the instance is already created, return the instance.
+        """
+        if cls._instance is None:
+            cls._instance = super(AppConfig, cls).__new__(cls)
+            cls._instance._load_config()
+        return cls._instance
+
+    def _load_config(self):
+        """Load the application configuration from a YAML file."""
+        path_config_yaml = config('PATH_CONFIG_YAML', default='config.yaml')
+        env = config('APP_ENV', default='sandbox')
+
+        with open(path_config_yaml, "r") as file:
+            config_data = yaml.safe_load(file)
+            self.env_config = config_data.get(env, {})
+
+            for key, value in self.env_config.items():
+                self.env_config[key] = config(value, default=value)
+
+    @property
+    def config(self) -> Dict[str, str]:
+        """Get the application configuration.
+
+        Returns:
+            Dict[str, str]: A dictionary with the application configuration.
+        """
+        return self.env_config
